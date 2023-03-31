78c9d776878a7898d20609d29c885bd934ec2021
Your Name <panigarhydeepakkumar27@gmail.com>
2023-03-31 23:43:43 +0530

    day-4

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,16 @@ const express=require('express')
 const cors=require("cors")
 const {connection}=require("./db")
 const {userRoute}=require("./routes/userroute")
+const {Router}=require("./routes/productroute")
+const {auth}=require("./middlware/auth")
 require("dotenv").config()
 const app=express()
 app.use(express.json())
 app.use(cors())
 
 app.use("/user",userRoute)
-
-
-
+// app.use(auth)
+app.use("/product",Router)
 
 
 
--- backend/models/product.js
+++ backend/models/product.js
@@ -1,5 +1,15 @@
 const mongoose=require("mongoose")
 const productSchema=mongoose.Schema({
+image:String,
+name:String,
+price:String,
+Ad_id:String,
+condition:String,
+Sellertype:String,
+brand:String,
+producttype:String,
+description:String
+
 
 })
 
